b1b1828ff91fb07be2bc9ed7741bd42ceaea8290
rcxz <rczxqq@yahoo.com>
2013-01-20 00:08:01 +0400

    finish cat command

--- test/cat_test.js
+++ test/cat_test.js
@@ -4,21 +4,45 @@ var assert = require('assert')
   , path = require('path')
 
 function assertStderr(command, msg, done) {
-  exec(command, function(error, stdout, stderr) {
+  exec(command, function(error, stdout, stderr){
     assert.equal(stderr, msg);
     done();
   })
 }
 
-describe('cat', function () {
+describe('cat', function(){
   var binfile = path.resolve('./bin/cat')
-    , command = function (args) { return 'node ' + binfile + ' ' + args };
+    , pwd = path.resolve('.')
+    , command = function(args){ return 'node ' + binfile + ' ' + args };
 
-  it('complains if file does not exist', function (done) {
-    var file = "/nowhere/file"
-      , msg = "\"" + file + "\" No such file"
+  it('complains if file does not exist', function(done){
+    var file = '/nowhere/file'
+      , msg = '"' + file + '" No such file'
 
     assertStderr(command(file), msg, done)
   })
-})
+
+  it('complains if file is a directory', function(done){
+    var file = '/tmp'
+      , msg = '"' + file + '" Is a directory'
+
+    assertStderr(command(file), msg, done);
+  })
+
+  it('concatenates files', function(done){
+    var factories = path.join(pwd, 'test', 'factories')
+      , a = path.join(factories, 'a')
+      , b = path.join(factories, 'b');
+
+    exec(command(a), function(error, stdout, stderr){
+      assert.equal(stdout, "foo\nbar\n\n");
+      done();
+    });
+
+    exec(command(a + ' ' + b), function(error, stdout, stderr){
+      assert.equal(stdout, "foo\nbar\n\nbaz\n");
+      done();
+    });
+  });
+});
 
